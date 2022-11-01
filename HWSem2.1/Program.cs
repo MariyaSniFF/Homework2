cc4ed501ba378af67b434242d1e034a0cd4427b4
Mariya <anteya@bk.ru>
2022-11-02 00:32:13 +0300

    Создана программа, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа

--- HWSem2.1/Program.cs
+++ HWSem2.1/Program.cs
@@ -1,6 +1,13 @@
 ﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 Console.WriteLine("Введите любое трехзначное число");
 int num = Int32.Parse(Console.ReadLine());
-int num1=num/10; 
-int num2=num1%10;
-Console.WriteLine(num2);
+if(num>=100 && num<=1000)
+{
+    int num1=num/10; 
+    int num2=num1%10;
+    Console.WriteLine(num2);
+}
+else 
+{
+    Console.WriteLine("Число не трёхзначное");
+}
---
+++ HWSem2.2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(100>num)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+ else {
+    while(num > 1000)
+    {
+        num/=10;
+    }
+        int result = num%10;
+
+Console.WriteLine($"Третья цифра {result}");
+     }
