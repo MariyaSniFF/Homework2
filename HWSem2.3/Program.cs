3c8a51e73166e0d90e33b00975fc968995155a81
Mariya <anteya@bk.ru>
2022-11-01 23:54:17 +0300

    Создана программа, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным

---
+++ HWSem2.3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.WriteLine("Введите номер дня недели");
+int num = Convert.ToInt32(Console.ReadLine());
+
+switch (num)
+{
+    case 1:
+        Console.WriteLine("Не входной");
+        break;
+    case 2:
+        Console.WriteLine("Не входной");
+        break;
+    case 3:
+        Console.WriteLine("Не входной");
+        break;
+    case 4:
+        Console.WriteLine("Не входной");
+        break;
+    case 5:
+        Console.WriteLine("Не входной");
+        break;
+    case 6:
+        Console.WriteLine("Выходной");
+        break;
+    case 7:
+        Console.WriteLine("Выходной");
+        break;
+    default:
+        Console.WriteLine("Цифра не обозначает день недели");
+        break;
+}
